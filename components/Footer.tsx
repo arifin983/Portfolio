4da34e580caa5962ab38f50dcdaa3e24f807504e
Muhammad Arif_Uddin <arifuddin.danin@gmail.com>
2024-07-10 22:40:23 +0500

    LinkedIn updated

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -54,7 +54,7 @@ const Footer = () => {
               <img src={socialMedia[1].img} alt="icons" width={20} height={20} />
             </div>
             </Link>
-          <Link href="https://www.linkedin.com/in/muhammad-arif-ud-din-a99273230/" target="blank">
+          <Link href="https://www.linkedin.com/in/arif-uddin-a99273230" target="blank">
             <div
               className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
             >
